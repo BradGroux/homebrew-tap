292f340a894ea86079abba9b54cf71f1034ae06c
Brad Groux <3053586+BradGroux@users.noreply.github.com>
2026-07-12 19:53:32 -0500

    chore: update Veritas Kanban cask to 5.2.2 (#23)
    Fixes #22

--- Casks/veritas-kanban.rb
+++ Casks/veritas-kanban.rb
@@ -1,6 +1,6 @@
 cask "veritas-kanban" do
-  version "5.2.1"
-  sha256 "a0b0a378a12f187295719bf945a6de2ce3025274017db417eaa4d3308fc3a757"
+  version "5.2.2"
+  sha256 "3f85d864a78251c8e1bb94f9914dcab0ce9795cf5a45f64fffdf33337cfb3d4a"
 
   url "https://github.com/BradGroux/veritas-kanban/releases/download/v#{version}/Veritas-Kanban-#{version}-mac-arm64.zip"
   name "Veritas Kanban"
